3f536aff749c6ff502ce7eacb540b2d3524b5495
Harrison Sayre <hsayre42@gmail.com>
2025-04-13 12:17:47 -0400

    Did ValidAnagram

---
+++ Anagram/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Integer> sChars = new HashMap<Character, Integer>();
+        HashMap<Character, Integer> tChars = new HashMap<Character, Integer>();
+
+        for(int i =0; i < s.length(); i++) {
+            sChars.put(s.charAt(i), sChars.getOrDefault(s.charAt(i), 0)+1);
+        }
+        for(int j =0; j < t.length(); j++) {
+            tChars.put(t.charAt(j), tChars.getOrDefault(t.charAt(j), 0)+1);
+        }
+
+        return sChars.equals(tChars);
+    }
+
+    public static void main(String[] args) {
+        Main test = new Main();
+        System.out.println(test.isAnagram("sat","tas"));
+        System.out.println(test.isAnagram("sat","sas"));
+        System.out.println(test.isAnagram("sat","tass"));
+    }
+}
