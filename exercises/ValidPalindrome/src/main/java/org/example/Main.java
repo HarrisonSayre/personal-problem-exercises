3f6d9ef076cb11e910500dfa9f1a0f475d0040c3
Harrison Sayre <hsayre42@gmail.com>
2025-04-20 19:44:14 -0400

    Used two pointer method for valid palindrome

---
+++ exercises/ValidPalindrome/src/main/java/org/example/Main.java
@@ -0,0 +1,32 @@
+package org.example;
+
+
+public class Main {
+
+    public boolean isPalindrome(String s) {
+
+        int i = 0;
+        int j = s.length()-1;
+
+        while(i < j) {
+            while(!Character.isAlphabetic(s.charAt(i))) {
+                i++;
+            }
+
+            while(!Character.isAlphabetic(s.charAt(j))) {
+                j--;
+            }
+            if(s.toLowerCase().charAt(i) != s.toLowerCase().charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Main test = new Main();
+        System.out.println(test.isPalindrome("a ,,,  ,bb   a"));
+    }
+}
