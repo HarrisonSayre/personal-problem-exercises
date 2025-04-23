7a4e808f5cf56cf9d2831d2c5d4e0c0a25c64cd2
Harrison Sayre <hsayre42@gmail.com>
2025-04-23 19:56:20 -0400

    Refactored to use hash for scaling/less repeated code.

--- exercises/ValidPrentheses/src/main/java/org/example/Main.java
+++ exercises/ValidPrentheses/src/main/java/org/example/Main.java
@@ -1,27 +1,22 @@
 package org.example;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 public class Main {
 
     public boolean isValid(String s) {
         Stack<Character> checkerStack = new Stack<Character>() ;
+        HashMap<Character, Character> parenTypes = new HashMap<Character,Character>();
+        parenTypes.put(')','(');
+        parenTypes.put(']','[');
+        parenTypes.put('}','{');
+
         for(char paren : s.toCharArray()){
             System.out.println(paren);
-            if(paren == ')'){
-                if(checkerStack.peek() != '('){
-                    return false;
-                }
-                checkerStack.pop();
-            }
-            else if(paren == ']'){
-                if(checkerStack.peek() != '['){
-                    return false;
-                }
-                checkerStack.pop();
-            }
-            else if(paren == '}'){
-                if(checkerStack.peek() != '{'){
+            if(parenTypes.containsKey(paren)){
+                System.out.println(checkerStack.peek());
+                if(checkerStack.peek() != parenTypes.get(paren)){
                     return false;
                 }
                 checkerStack.pop();
