75f13ef63110cb19f1c506c13648f341323cf305
Harrison Sayre <hsayre42@gmail.com>
2025-07-03 18:15:47 -0400

    Generated valid parentheses using a dfs and a list of valid strings.

---
+++ exercises/GenerateParentheses/src/main/java/org/example/Main.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+
+    public void dfsParens (int open, int close, int total, String currentCombo, List<String> parenCombos){
+
+        if(close > open || open > total)
+        {
+            return;
+        }
+
+        if(open == close && open == total)
+        {
+            //System.out.println(currentCombo);
+            parenCombos.add(currentCombo);
+            return;
+        }
+
+        dfsParens(open+1, close, total, currentCombo+"(", parenCombos);
+        dfsParens(open, close+1, total, currentCombo+")", parenCombos);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        List<String> validParentheses = new ArrayList<>();
+
+        if(n <= 0)
+        {
+            validParentheses.add("");
+            return validParentheses;
+        }
+
+        dfsParens(0, 0, n, "", validParentheses);
+
+        //System.out.println(validParentheses);
+
+        return validParentheses;
+    }
+
+    public static void main(String[] args) {
+
+        Main test = new Main();
+        System.out.println(test.generateParenthesis(3));
+    }
+}
